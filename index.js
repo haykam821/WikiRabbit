5e5a60f521f2c817e936dde524923c02de0a23d4
haykam821 <haykam821@users.noreply.github.com>
2018-02-01 19:08:19 -0500

    try

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 const wikipedia = require("node-wikipedia");
 
-wikipedia.page.data("Wikipedia", {
-  content: true
-}, function(response) {
-  console.log(response)
-});
+var flattenObject = function(ob) {
+  var toReturn = {};
+  var flatObject;
+  for (var i in ob) {
+    if (!ob.hasOwnProperty(i)) {
+      continue;
+    }
+    if ((typeof ob[i]) === 'object') {
+      flatObject = flattenObject(ob[i]);
+      for (var x in flatObject) {
+        if (!flatObject.hasOwnProperty(x)) {
+          continue;
+        }
+        toReturn[i + (!!isNaN(x) ? '.' + x : '')] = flatObject[x];
+      }
+    } else {
+      toReturn[i] = ob[i];
+    }
+  }
+  return toReturn;
+};
+
+function getArray() {
+  wikipedia.page.data("Wikipedia", {
+    content: true
+  }, function(response) {
+    let linkTitles = response.links.map(item => {
+      return item["*"];
+    });
+
+    //console.log(linkTitles.join("\n"));
+  });
+};
